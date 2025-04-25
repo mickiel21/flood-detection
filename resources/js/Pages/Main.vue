44436ec44d345dec86195fcc61efabfee7b84665
moraa <mickieljohnoraa@gmail.com>
2025-04-25 22:32:48 +0800

    add landing page assets and design

---
+++ resources/js/Pages/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    
+    <div class="app-main" id="parent">
+        <div class="header">
+            <h4>Get Weather</h4>
+        </div>
+        <div class="searchInputBox">
+            <input type="text" name="" id="input-box" class="input-box" placeholder="Enter city name">
+            <button id="btn" class="btn">Search</button>
+        </div>
+        <div class="weather-body" id="weather-body">
+        </div>
+            
+    </div>
+    
+</template>
+
+<script>
+
+
+export default {
+    name: 'Dashboard',
+    components: {
+        
+    },
+    data() {
+        return {
+        }
+    }
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,13 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::get('/main', function () {
+    return Inertia::render('Main');
+})->name('main');
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
